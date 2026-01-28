1cd5bcd432bea04e118cfd03d6005e203770a1b9
Cameron Lee <7cameronlee@gmail.com>
2026-01-27 18:29:20 -0800

    Initial commit and starting Prelab 1

---
+++ Lab1/Lab1.ino
@@ -0,0 +1,47 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// ================== Task Sheet 6a =======================
+
+void turnLeft(float duration, float speed){
+
+}
+
+void turnRight(float duration, float speed){
+
+}
+
+void Halt(){
+
+}
+
+void moveForward(float distance, float speed){
+
+}
+
+void moveBackward(float distance, float speed){
+
+}
+
+void moveForwardTurningLeft(float distance, float speed){
+
+}
+
+void moveForwardTurningRight(float distance, float speed){
+
+}
+
+void moveBackwardTurningLeft(float distance, float speed){
+
+}
+
+void moveBackwardTurningRight(float distance, float speed){
+
+}
