dd554ae5d60034fd3e594691339d6e35ed89c51e
Cameron Lee <7cameronlee@gmail.com>
2026-02-18 19:32:31 -0800

    Prelab 4 complete

--- lab4/lab4.ino
+++ lab4/lab4.ino
@@ -9,14 +9,16 @@ using namespace Pololu3piPlus32U4;
 #define diaR  3.2
 #define nL 12
 #define nR 12
-#define w 9.6
+#define w 96 //distance between two wheels in mm
 #define gearRatio 75
 
 //Update kp and kd based on your testing
-#define minOutput -100
-#define maxOutput 100
-#define kp ...
-#define base_speed 50
+#define minOutput -2.0
+#define maxOutput 2.0
+#define kp 0.1 //radians per second per cm
+#define base_speed 50 //50mm/s
+
+#define MM_IN_CM 10
 
 Motors motors;
 Servo servo;
@@ -27,10 +29,12 @@ Pcontroller Pcontroller (kp, minOutput, maxOutput);
 const double distFromWall=10.0; // Goal distance from wall (cm)
 
 double wallDist;
+double Pout;
 
 void setup() {
   Serial.begin(9600);
   servo.attach(5);
+  servo.write(0);
   delay(40);
   //Move Sonar to desired direction using Servo
 }
@@ -40,7 +44,7 @@ void loop() {
   wallDist = sonar.readDist();
 
   //UNCOMMENT AFTER IMPLEMENTING Pcontroller
-  //Pout = Pcontroller.update(wallDist, distFromWall); //uncomment if using Pcontroller 
+  Pout = Pcontroller.update(wallDist, distFromWall); //uncomment if using Pcontroller 
 
   //(LAB 4 - TASK 3.1) IMPLEMENT PCONTROLLER 
   
@@ -56,6 +60,31 @@ void loop() {
   Hint: Also use baseSpeed when setting motor speeds*/
 
   //Also print outputs to serial monitor for testing purposes
+  Serial.print("Distance from wall: ");
+  Serial.print(wallDist);
+  Serial.println("cm\n");
+  Serial.print("Angular Velocity: ");
+  Serial.print(Pout);
+  Serial.println("rad/s");
+ 
+  int leftVelocity = (int)constrain(calculateLeftWheelVelocity(base_speed, Pout * MM_IN_CM), -400, 400);
+  int rightVelocity = (int)constrain(calculateRightWheelVelocity(base_speed, Pout * MM_IN_CM), -400, 400);
+  motors.setSpeeds(leftVelocity, rightVelocity);
 
+  Serial.print("Left velocity: ");
+  Serial.println(leftVelocity);
+  Serial.print("Right Velocity: ");
+  Serial.println(rightVelocity);
+  Serial.println("-----\n");
+  
+}
 
+double calculateRightWheelVelocity(double baseSpeed, double angularVelocity){
+  double v = baseSpeed + ((w/2) * angularVelocity);
+  return v;
+}
+
+double calculateLeftWheelVelocity(double baseSpeed, double angularVelocity){
+  double v = baseSpeed - ((w/2) * angularVelocity);
+  return v;
 }
